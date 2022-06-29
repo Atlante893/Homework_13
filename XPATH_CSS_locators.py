7ca371ca8c3e4a6b683d3228dbad0fef47014b04
Atlante893 <64986754+Atlante893@users.noreply.github.com>
2022-06-30 00:16:00 +0300

    Add files via upload

---
+++ XPATH_CSS_locators.py
@@ -0,0 +1,44 @@
+#https://bpa.com.ua/
+
+#XPATH
+#1.  //div[@class='topBar-submenu-btn']/ancestor::div[@class='topBar-right']
+#2.  //body[@class='home page-template-default page page-id-830']/ancestor-or-self::*
+#3.  //script/attribute::charset
+#4.  //div[@class='titlemain-buttons']/child::a[@class='btn-general btn-outline']
+#5.  //div [@class='topBar-wrap']//div//a[@href='tel:+380672196701']
+#6.  //form[@id='form']/descendant::*
+#7.  //*[@id="services"]/div/div/div[1]
+#8.  //button[@id='submit-form']/descendant-or-self::*
+#9.  //jdiv[@class='pseudoHeight_d332']/following::*
+#10. //jdiv[@class='pseudoHeight_d332']/following-sibling::*
+#11. //jdiv[@class='pseudoHeight_d332']/parent::*
+#12. //body/preceding::*
+#13. //div[@class='globalwrap']/parent::*
+#14. //div[@class and @style and @data-co]
+#15. //title[text()='Бухгалтерская компания в Киеве - b.p.a.']
+#16. //title[contains(text(),'Бухгалтерская ')]
+#17. //div[contains(@class,'header ')]
+#18. //div[@class='globalwrap']//div[contains(@class,'services ')]//div[@class='services-list']
+#20. //div[@class='services-list']//h3[contains(@class,'title center')]
+#21. //form[@id='form']//input[@name='name']
+#22. //form[@id='form']//input[@name='phone']
+#23. //div[contains(@class,'services ')]//form[@id='form']//input[@name='email']
+#24. //form[@id='form']//input[@name='required-field']
+#25. //div[contains(@style,'transform: translate3d')]
+#26. //div[@class='owl-item active']//p[contains(text(),'ВИРТУОЗ')]
+#27. //div[@class='owl-item cloned']//p[contains(text(),'ВИРТУОЗ')]
+#28. //h2[text()='Кому мы полезны']
+#29. //h3[@class='services-item-title']/child::span[contains(text(),'Юридические услуги')]
+#30. //*[contains(text(),'Регистрация бизнеса')]
+
+#CSS
+#1.  div [class='topBar-wrap'] div a[href='tel:+380672196701']
+#2.  div[class='titlemain-buttons'] > a[class='btn-general btn-outline']
+#3.  div[class][style][data-co]
+#4.  div[class*='header ']
+#5.  div[class='globalwrap'] div[class*='services '] div[class='services-list']
+#6.  div[class='services-list'] h3[class*='title center']
+#7.  form[id='form'] input[name='name']
+#8.  div[class*='services '] form[id='form'] input[name='email']
+#9.  form[id='form'] input[name='required-field']
+#10. div[style*='transform: translate3d']
